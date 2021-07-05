7fad3571544b8ad3a9cbeecae1a218e1b4f43ca3
Aleksey Koshkin <ekbdizzy@yandex.ru>
2021-07-05 17:43:05 +0300

    Change owners_phonenumber field to owner_phone_number

--- property/models.py
+++ property/models.py
@@ -6,7 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_phone_number = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер телефона', blank=True, null=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
